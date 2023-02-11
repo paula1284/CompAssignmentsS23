0f341f9ad5a2d7e7c0bb0bf551bf4f36b9f84925
Kevin Luo <44309027+kevinzluo@users.noreply.github.com>
2023-02-11 12:56:02 -0500

    remove old assignments

--- reactlab/content/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('contrib/<int:contrib_id>/', views.display_contributor, name = "display_contributor"),
-    path('article/<slug:slug>/', views.display_article, name = "display_article")
-]
